b8cb6014e3067582e62c8287648ef4fec9e0c0a6
Friedmann <friedmanngerber@outlook.com>
2015-09-18 14:30:14 -0300

    teste com json

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var githubhook = require('githubhook');
-var github = githubhook({
-});
+var github = githubhook({ });
+
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'repos-enabled/sabordoc.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
 
 github.listen();
 
 github.on('*', function (event, repo, ref, data) {
 
-    //if(data.action == 'closed'){
-        var exec = require('child_process').exec;
+    loadJSON(function(response) {
+        var json_data = JSON.parse(response);
 
-        exec('cd /var/www/' + repo + ' && git pull origin develop');
-    //}
+        if(data.action == 'closed' && data.pull_request.merged == 'true'){
+            var exec = require('child_process').exec;
+
+            exec('cd /var/www/' + json_data.repo + ' && git pull origin ' + json_data.branch);
+        }
+    });
 });
