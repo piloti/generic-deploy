a3d23518d731194348838fb7b5a66702e2ebb695
Friedmann <friedmanngerber@outlook.com>
2015-09-18 16:33:38 -0300

    sem json

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 var githubhook = require('githubhook');
 var github = githubhook({ });
 
-function loadJSON(callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'repos-enabled/sabordoc.json', true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-
 github.listen();
 
 github.on('*', function (event, repo, ref, data) {
 
-    loadJSON(function(response) {
-        var json_data = JSON.parse(response);
+    if(data.action == 'closed' && data.pull_request.merged == 'true'){
+        var exec = require('child_process').exec;
 
-        if(data.action == 'closed' && data.pull_request.merged == 'true'){
-            var exec = require('child_process').exec;
-
-            exec('cd /var/www/' + json_data.repo + ' && git pull origin ' + json_data.branch);
-        }
-    });
+        exec('cd /var/www/' + repo + ' && git pull origin develop');
+    }
 });
